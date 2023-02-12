3e7c4a6f68b86b23400f46b7f61e4eba5a732504
RimmaM <rimma.maksimova@gmail.com>
2023-02-12 13:43:31 +0300

    add 2 files

---
+++ pythonProject/05_02_list.py
@@ -0,0 +1,133 @@
+l1 = [1,2,3,4,5]
+#print(type(l1))
+
+names = ['Anna','Alex','Victor']
+#print(type(names))
+
+# -------------------------------------------------
+
+l1 = []
+l2 = list()
+l3 = [9, 3.14, 'text', True, False, [1,6,3], {'name':'Anna'}]
+#print(type(l3))
+
+item1 = 'yolochka'
+
+#print(item1[1])
+
+l4 = list(item1)
+#print(l4)
+
+# -------------------------------------------------
+
+l5 =['Anna', 'Elena', 7]
+l55 = l5*4
+#print(l55)
+
+# -------------------------------------------------
+
+# users = ['Alex', 'yolochka', 'Victor']
+# print(users[0])
+# print(users[1])
+# print(users[2])
+
+# print(users[-1])
+# print(users[-2])
+# print(users[-3])
+
+# -------------------------------------------------
+
+# users = ['Alex', 'yolochka', 'Anna']
+# users[1]='Victor'
+# print(users)
+
+# u1, u2, u3 = users
+# print(u3)
+
+# for user in users:
+#     print(user)
+
+# print(len(users))
+# i = 0
+# while i < len(users):
+#     print(users[i])
+#     i+=1;
+
+# -------------------------------------------------
+
+# l1 = [1,2,3]
+# l2 = [1,3,2]
+#
+# if l1==l2:
+#     print('1  ---  l1 = l2')
+# else:
+#     print('2  ---  l1 != l2')
+
+# -------------------------------------------------
+
+users = ['Alex',
+         'Anna',
+         'yolochka',
+         'yolochka',
+         'Victor',
+         'Elens',
+         'Vadim',
+         'Sergey',
+         'Dima',
+         'Inna']
+
+# print('lehght of users == ',len(users))
+# del users[1::2]
+# print(users)
+
+items = [4,6,1,0,-9,7,9]
+print(max(items))
+
+# print(items)
+# items.sort()
+# print(items)
+# items.reverse()
+# print(items)
+#
+# users1 = ['Anna', 'Alex', 'Victor']
+# users2 = users1
+# users2.append('Elena')
+# print(users1)
+# print(users2)
+#
+# print('================')
+#
+# users1 = ['Anna', 'Alex', 'Victor']
+# users2 = users1.copy()
+# users2.append('Elena')
+# print(users1)
+# print(users2)
+
+
+
+
+# users.append(77)
+# print(users)
+
+# users.insert(1,77)
+# print(users)
+
+# users.extend(['Elene', 'Vika'])
+# print(users)
+#
+# index_item = users.index('yolochka')
+# print(index_item)
+#
+# users_remove = users.pop(index_item)
+# print(users)
+
+# print(users)
+# users.pop()
+# print(users)
+
+# -------------------------------------------------
+
+
+
+
+# -------------------------------------------------
